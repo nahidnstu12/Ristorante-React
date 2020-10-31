a70d0f79378ee2fa6ddf00d436e3fc7701d066ce
= <=>
2020-10-31 21:11:12 +0600

    Updates

---
+++ precache-manifest.f3972a901c0ed49b81100ac8265446ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a2ade3a08ad0206673df005113b546",
+    "url": "/Ristorante-React/index.html"
+  },
+  {
+    "revision": "16fc5d2ecc1786c67bdc",
+    "url": "/Ristorante-React/static/css/2.eeaa5884.chunk.css"
+  },
+  {
+    "revision": "17741f5b97895551431f",
+    "url": "/Ristorante-React/static/css/main.bc809497.chunk.css"
+  },
+  {
+    "revision": "16fc5d2ecc1786c67bdc",
+    "url": "/Ristorante-React/static/js/2.278481c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ristorante-React/static/js/2.278481c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17741f5b97895551431f",
+    "url": "/Ristorante-React/static/js/main.49c36b9c.chunk.js"
+  },
+  {
+    "revision": "98a2da61ea3b4256792f",
+    "url": "/Ristorante-React/static/js/runtime-main.8a3300ef.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
